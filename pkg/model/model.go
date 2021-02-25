aa05859d36eb0b28e63f417093c3875d1ae3d85c
alseiitov <dossan.alseiit@gmail.com>
2021-02-25 18:53:03 +0600

    .

---
+++ pkg/model/model.go
@@ -0,0 +1,92 @@
+package model
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type ScoreBoard []Score
+
+type Score struct {
+	Player string `json:"player"`
+	Score  int    `json:"score"`
+	Time   int    `json:"time"`
+}
+
+func (s *Score) ReadFromBytes(data []byte) error {
+	err := json.Unmarshal(data, s)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Score) WriteToFile(fileName string) error {
+	var scoreBoard ScoreBoard
+	err := scoreBoard.ReadFromFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	found := false
+	for i, v := range scoreBoard {
+		if s.Player == v.Player {
+			found = true
+			if s.Score > v.Score {
+				scoreBoard[i] = s
+			}
+		}
+	}
+
+	if !found {
+		scoreBoard = append(scoreBoard, s)
+	}
+
+	data, err := json.Marshal(scoreBoard)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(fileName, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s ScoreBoard) Sort() {
+	l := len(s)
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-i-1; j++ {
+			if s[j].Score < s[j+1].Score {
+				s[j], s[j+1] = s[j+1], s[j]
+			}
+			if s[j].Score == s[j+1].Score && s[j].Time > s[j+1].Time {
+				s[j], s[j+1] = s[j+1], s[j]
+			}
+		}
+	}
+}
+
+func (s ScoreBoard) ToBytes() ([]byte, error) {
+	data, err := json.Marshal(s)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (s *ScoreBoard) ReadFromFile(fileName string) error {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- website/src/js/game.js
+++ website/src/js/game.js
@@ -1,3 +1,5 @@
+const API_ADRESS = 'http://localhost:8080/api/scoreboard'
+
 const KEY_CODE_LEFT = 37;
 const KEY_CODE_RIGHT = 39;
 const KEY_CODE_SPACE = 32;
@@ -18,8 +20,11 @@ const ENEMY_VERTICAL_SPACING = 80;
 const ENEMY_COOLDOWN = 4.0;
 
 
+
 const GAME_STATE = {
+    startTime: Date.now(),
     lastTime: Date.now(),
+    time: 0,
     leftPressed: false,
     rightPressed: false,
     spacePressed: false,
@@ -33,10 +38,9 @@ const GAME_STATE = {
     gameOver: false,
     score: 0,
     pause: false,
-    timer: "00:00"
 }
 
-function rectsIntersect(r1, r2){
+function rectsIntersect(r1, r2) {
     return !(
         r2.left > r1.right ||
         r2.right < r1.left ||
@@ -45,16 +49,16 @@ function rectsIntersect(r1, r2){
     );
 }
 
-function setPositions($el, x, y){
+function setPositions($el, x, y) {
     $el.style.transform = `translate(${x}px, ${y}px`;
 }
 
-function clamp(v, min, max){
-    if (v < min){
+function clamp(v, min, max) {
+    if (v < min) {
         return min;
-    }else if (v > max){
+    } else if (v > max) {
         return max
-    }else{
+    } else {
         return v
     }
 }
@@ -65,104 +69,104 @@ function rand(min, max) {
     return min + Math.random() * (max - min);
 }
 
-function onKeyDown(e){
-    if (e.keyCode === KEY_CODE_LEFT){
+function onKeyDown(e) {
+    if (e.keyCode === KEY_CODE_LEFT) {
         GAME_STATE.leftPressed = true;
-    }else if (e.keyCode === KEY_CODE_RIGHT){
+    } else if (e.keyCode === KEY_CODE_RIGHT) {
         GAME_STATE.rightPressed = true;
-    }else if (e.keyCode === KEY_CODE_SPACE){
+    } else if (e.keyCode === KEY_CODE_SPACE) {
         GAME_STATE.spacePressed = true;
-    }else if (e.keyCode === KEY_CODE_PAUSE){
-        if (!GAME_STATE.pause){
+    } else if (e.keyCode === KEY_CODE_PAUSE) {
+        if (!GAME_STATE.pause) {
             GAME_STATE.pause = true;
-        }else{
+        } else {
             GAME_STATE.pause = false;
         }
     }
 }
 
-function onKeyUp(e){
-    if (e.keyCode === KEY_CODE_LEFT){
+function onKeyUp(e) {
+    if (e.keyCode === KEY_CODE_LEFT) {
         GAME_STATE.leftPressed = false;
-    }else if (e.keyCode === KEY_CODE_RIGHT){
+    } else if (e.keyCode === KEY_CODE_RIGHT) {
         GAME_STATE.rightPressed = false;
-    }else if (e.keyCode === KEY_CODE_SPACE){
+    } else if (e.keyCode === KEY_CODE_SPACE) {
         GAME_STATE.spacePressed = false;
     }
 }
 
 
 // PLAYER 
-function createPlayer($container){
+function createPlayer($container) {
     GAME_STATE.playerX = GAME_WIDTH / 2;
-    GAME_STATE.playerY = GAME_HEIGHT -50;
+    GAME_STATE.playerY = GAME_HEIGHT - 50;
     const $player = document.createElement("img");
-    $player.src = "img/player-blue-1.png";
+    $player.src = "/src/img/player-blue-1.png";
     $player.className = "player";
     $container.appendChild($player);
     setPositions($player, GAME_STATE.playerX, GAME_STATE.playerY);
 }
 
-function updatePlayer(dt, $container){
-    if (GAME_STATE.leftPressed){
+function updatePlayer(dt, $container) {
+    if (GAME_STATE.leftPressed) {
         GAME_STATE.playerX -= dt * PLAYER_MAX_SPEED;
-    }else if (GAME_STATE.rightPressed){
+    } else if (GAME_STATE.rightPressed) {
         GAME_STATE.playerX += dt * PLAYER_MAX_SPEED;
     }
 
     GAME_STATE.playerX = clamp(GAME_STATE.playerX, PLAYER_WIDTH, GAME_WIDTH - PLAYER_WIDTH)
 
-    if (GAME_STATE.spacePressed &&  GAME_STATE.playerCooldown <= 0 ){
+    if (GAME_STATE.spacePressed && GAME_STATE.playerCooldown <= 0) {
         createLaser($container, GAME_STATE.playerX, GAME_STATE.playerY);
         GAME_STATE.playerCooldown = LASER_COOLDOWN;
     }
-    if (GAME_STATE.playerCooldown > 0){
+    if (GAME_STATE.playerCooldown > 0) {
         GAME_STATE.playerCooldown -= dt;
     }
     const $player = document.querySelector(".player")
     setPositions($player, GAME_STATE.playerX, GAME_STATE.playerY)
 }
 
-function destroyPlayer($container, player){
+function destroyPlayer($container, player) {
     $container.removeChild(player);
     GAME_STATE.gameOver = true;
 }
 
 function playerHasWon() {
     return GAME_STATE.enemies.length === 0;
-  }
-  
+}
+
 
 // LASER
-function createLaser($container, x, y){
+function createLaser($container, x, y) {
     const $element = document.createElement("img");
-    $element.src = "img/laser-blue-1.png";
+    $element.src = "/src/img/laser-blue-1.png";
     $element.className = "laser";
     $container.appendChild($element);
-    const laser = { x, y, $element};
+    const laser = { x, y, $element };
     GAME_STATE.lasers.push(laser);
     setPositions($element, x, y);
-    const audio = new Audio("sound/sfx-laser1.ogg");
+    const audio = new Audio("/src/sound/sfx-laser1.ogg");
     audio.play();
 }
 
 
-function updateLasers(dt, $container){
+function updateLasers(dt, $container) {
     const lasers = GAME_STATE.lasers;
-    for (let i = 0; i < lasers.length; i++){
+    for (let i = 0; i < lasers.length; i++) {
         const laser = lasers[i];
         laser.y -= dt * LASER_MAX_SPEED;
-        if (laser.y < 0){
+        if (laser.y < 0) {
             destroyLaser($container, laser)
         }
         setPositions(laser.$element, laser.x, laser.y)
         const r1 = laser.$element.getBoundingClientRect();
         const enemies = GAME_STATE.enemies;
-        for (let j = 0; j < enemies.length; j++){
+        for (let j = 0; j < enemies.length; j++) {
             const enemy = enemies[j];
             if (enemy.isDead) continue;
             const r2 = enemy.$element.getBoundingClientRect();
-            if (rectsIntersect(r1, r2)){
+            if (rectsIntersect(r1, r2)) {
                 destroyEnemy($container, enemy);
                 destroyLaser($container, laser)
             }
@@ -174,30 +178,30 @@ function updateLasers(dt, $container){
 
 
 ////////////// ENEMY LASER
-function createEnemyLaser($container, x, y){
+function createEnemyLaser($container, x, y) {
     const $element = document.createElement("img");
-    $element.src = "img/laser-red-5.png";
+    $element.src = "/src/img/laser-red-5.png";
     $element.className = "enemy-laser";
     $container.appendChild($element);
-    const laser = {x, y, $element};
+    const laser = { x, y, $element };
     GAME_STATE.enemyLasers.push(laser);
     setPositions($element)
 }
 
-function updateEnemyLasers(dt, $container){
+function updateEnemyLasers(dt, $container) {
     const lasers = GAME_STATE.enemyLasers;
-    for (let i = 0; i < lasers.length; i++){
+    for (let i = 0; i < lasers.length; i++) {
         laser = lasers[i];
         laser.y += dt * LASER_MAX_SPEED;
-        if (laser.y + 30  > GAME_HEIGHT ){
+        if (laser.y + 30 > GAME_HEIGHT) {
             destroyLaser($container, laser);
         }
         setPositions(laser.$element, laser.x, laser.y);
         const r1 = laser.$element.getBoundingClientRect();
         const player = document.querySelector(".player");
         const r2 = player.getBoundingClientRect();
-        if (rectsIntersect(r1, r2)){
-            if (GAME_STATE.playerLifes === 0){
+        if (rectsIntersect(r1, r2)) {
+            if (GAME_STATE.playerLifes === 0) {
                 destroyPlayer($container, player);
                 break;
             }
@@ -209,37 +213,37 @@ function updateEnemyLasers(dt, $container){
     GAME_STATE.enemyLasers = GAME_STATE.enemyLasers.filter(e => !e.isDead)
 }
 
-function destroyLaser($container, laser){
+function destroyLaser($container, laser) {
     $container.removeChild(laser.$element)
     laser.isDead = true
 }
 
 
 // ENEMY
-function createEnemy($container, x, y){
+function createEnemy($container, x, y) {
     const $element = document.createElement("img");
-    $element.src = "img/enemy-blue-1.png";
+    $element.src = "/src/img/enemy-blue-1.png";
     $element.className = "enemy";
     $container.appendChild($element)
-    const enemy = {x, y,cooldown: rand(0.5, ENEMY_COOLDOWN), $element}
+    const enemy = { x, y, cooldown: rand(0.5, ENEMY_COOLDOWN), $element }
     GAME_STATE.enemies.push(enemy)
     setPositions($element, x, y)
 }
 
 
-function updateEnemy(dt, $container){
+function updateEnemy(dt, $container) {
     const dx = Math.sin(GAME_STATE.lastTime / 1000.0) * 50;
     const dy = Math.cos(GAME_STATE.lastTime / 1000.0) * 10;
 
     const enemies = GAME_STATE.enemies
 
-    for (let i = 0; i < enemies.length; i++){
+    for (let i = 0; i < enemies.length; i++) {
         const enemy = enemies[i];
         const x = enemy.x + dx;
         const y = enemy.y + dy;
         setPositions(enemy.$element, x, y);
         enemy.cooldown -= dt;
-        if (enemy.cooldown <= 0){
+        if (enemy.cooldown <= 0) {
             createEnemyLaser($container, x, y);
             enemy.cooldown = ENEMY_COOLDOWN;
         }
@@ -248,7 +252,7 @@ function updateEnemy(dt, $container){
     GAME_STATE.enemies = GAME_STATE.enemies.filter(e => !e.isDead);
 }
 
-function destroyEnemy($container, enemy){
+function destroyEnemy($container, enemy) {
     $container.removeChild(enemy.$element);
     enemy.isDead = true;
     GAME_STATE.score += 10;
@@ -257,7 +261,7 @@ function destroyEnemy($container, enemy){
 
 
 ////////////////////////////////////////////////////////////////////////
-function init(){
+function init() {
 
     document.getElementById("score").innerHTML = GAME_STATE.score
     document.getElementById("life").innerHTML = GAME_STATE.playerLifes
@@ -266,9 +270,9 @@ function init(){
     createPlayer($container);
 
     const enemySpacing = (GAME_WIDTH - ENEMY_HORIZONTAL_PADDING * 2) / (ENEMIES_PER_ROW - 1);
-    for (let i = 0; i < 3; i++){
+    for (let i = 0; i < 3; i++) {
         const y = ENEMY_VERTICAL_PADDING + i * ENEMY_VERTICAL_SPACING;
-        for (let j = 0; j < ENEMIES_PER_ROW; j++){
+        for (let j = 0; j < ENEMIES_PER_ROW; j++) {
             const x = j * enemySpacing + ENEMY_HORIZONTAL_PADDING;
             createEnemy($container, x, y)
         }
@@ -276,27 +280,29 @@ function init(){
 }
 
 
-function reset(){
+function reset() {
 
     const gameField = document.querySelector(".game")
 
-    if (document.querySelector(".player") !== null){
+    if (document.querySelector(".player") !== null) {
         destroyPlayer(gameField, document.querySelector(".player"))
     }
 
-    for (let i = 0; i < GAME_STATE.enemies.length; i++){
+    for (let i = 0; i < GAME_STATE.enemies.length; i++) {
         destroyEnemy(gameField, GAME_STATE.enemies[i]);
     }
 
-    for (let i = 0; i < GAME_STATE.lasers.length; i++){
+    for (let i = 0; i < GAME_STATE.lasers.length; i++) {
         destroyLaser(gameField, GAME_STATE.lasers[i]);
     }
 
-    for (let i = 0; i < GAME_STATE.enemyLasers.length; i++){
+    for (let i = 0; i < GAME_STATE.enemyLasers.length; i++) {
         destroyLaser(gameField, GAME_STATE.enemyLasers[i]);
     }
 
+    GAME_STATE.startTime = Date.now();
     GAME_STATE.lastTime = Date.now();
+    GAME_STATE.time = 0;
     GAME_STATE.leftPressed = false;
     GAME_STATE.rightPressed = false;
     GAME_STATE.spacePressed = false;
@@ -312,33 +318,36 @@ function reset(){
     GAME_STATE.pause = false;
 }
 
-function update(){
-    
+function update() {
+
     const currentTime = Date.now();
     const $container = document.querySelector(".game");
     document.getElementById("time").innerHTML = 0
     const dt = (currentTime - GAME_STATE.lastTime) / 1000;
     GAME_STATE.lastTime = currentTime;
-    
-    if(GAME_STATE.gameOver){
-        document.querySelector('.game-over').style.display = "block";
+
+    GAME_STATE.time = parseInt((currentTime - GAME_STATE.startTime) / 1000)
+    document.getElementById("time").innerHTML = formatTime(GAME_STATE.time)
+
+    if (GAME_STATE.gameOver) {
+        lose()
         return;
     }
 
     if (playerHasWon() && document.querySelector(".player") !== null) {
-        document.querySelector(".congratulations").style.display = "block";
+        win()
         return;
-    }    
+    }
 
-    if (!GAME_STATE.pause){
+    if (!GAME_STATE.pause) {
         updatePlayer(dt, $container);
         updateLasers(dt, $container);
         updateEnemy(dt, $container);
         updateEnemyLasers(dt, $container);
-    }else{
+    } else {
         document.querySelector(".pause").style.display = "block";
     }
-    
+
     window.requestAnimationFrame(update);
 }
 
@@ -347,45 +356,15 @@ function update(){
 window.addEventListener("keydown", onKeyDown);
 window.addEventListener("keyup", onKeyUp);
 
-
-
-
-
-
-
-
-/////////////////////////// get add score
-function getScoreBoard(skip) {
-    fetch('http://localhost:8080/api/scoreboard', {
-      method: 'get'
-    }).then(res => res.json())
-      .then(res => parseScores(res, skip));
-}
-  
-function parseScores(res, skip){
-    console.log(Date.now())
-    const table = document.querySelector(".scoreboard-table")
-    let tableContent = table.innerHTML
-    for (let i = skip; i < skip + 10; i++){
-        tableContent += '<tr><td>' + (i +  1)  + '</td><td>' + res[i].player + '</td><td>' + res[i].score + '</td><td>' + res[i].time + '</td></tr>'
-    }
-    table.innerHTML = tableContent
-}
-
-
-
-
-
-
-
 ///////////////////////// Main menu and buttons
-function start(){
+function start() {
     reset()
     clearDisplay();
     document.querySelector(".main-menu").style.display = "block";
 }
 
 document.querySelector(".start-game").addEventListener("click", (e) => {
+    GAME_STATE.startTime = Date.now();
     clearDisplay();
     init()
     update()
@@ -414,6 +393,11 @@ document.querySelector(".back-to-menu-from-game").addEventListener("click", (e) 
     start()
 })
 
+////////////////////////// Score
+document.querySelector(".submit").addEventListener("click", (e) => {
+    postScore()
+})
+
 ////////////////////////// Scoreboard
 document.querySelector(".back-to-menu-from-scoreboard").addEventListener("click", (e) => {
     start()
@@ -424,32 +408,34 @@ document.querySelector(".back-to-menu-from-scoreboard").addEventListener("click"
 document.querySelector(".pagination_1").addEventListener("click", (e) => {
     const table = document.querySelector(".scoreboard-table")
     table.innerHTML = '<tr><th>' + 'Rank' + '</th><th>' + 'Name' + '</th><th>' + 'Score' + '</th><td>' + 'Time' + '</td></tr>'
-    getScoreBoard(skip=0)
+    getScoreBoard(skip = 0)
 })
 document.querySelector(".pagination_2").addEventListener("click", (e) => {
     const table = document.querySelector(".scoreboard-table")
     table.innerHTML = '<tr><th>' + 'Rank' + '</th><th>' + 'Name' + '</th><th>' + 'Score' + '</th><td>' + 'Time' + '</td></tr>'
-    getScoreBoard(skip=10)
+    getScoreBoard(skip = 10)
 })
 document.querySelector(".pagination_3").addEventListener("click", (e) => {
     const table = document.querySelector(".scoreboard-table")
     table.innerHTML = '<tr><th>' + 'Rank' + '</th><th>' + 'Name' + '</th><th>' + 'Score' + '</th><td>' + 'Time' + '</td></tr>'
-    getScoreBoard(skip=20)
+    getScoreBoard(skip = 20)
 })
 document.querySelector(".pagination_4").addEventListener("click", (e) => {
     const table = document.querySelector(".scoreboard-table")
     table.innerHTML = '<tr><th>' + 'Rank' + '</th><th>' + 'Name' + '</th><th>' + 'Score' + '</th><td>' + 'Time' + '</td></tr>'
-    getScoreBoard(skip=30)
+    getScoreBoard(skip = 30)
 })
 document.querySelector(".pagination_5").addEventListener("click", (e) => {
     const table = document.querySelector(".scoreboard-table")
     table.innerHTML = '<tr><th>' + 'Rank' + '</th><th>' + 'Name' + '</th><th>' + 'Score' + '</th><td>' + 'Time' + '</td></tr>'
-    getScoreBoard(skip=40)
+    getScoreBoard(skip = 40)
 })
 /////////////////////////// Win
-function win(){
+function win() {
     clearDisplay();
     document.querySelector(".congratulations").style.display = "block";
+    document.querySelector(".name-input").style.display = "block";
+    document.querySelector(".name-input").style.background = "#c7a526";
 }
 
 document.querySelector(".restart-win").addEventListener("click", (e) => {
@@ -463,9 +449,11 @@ document.querySelector(".back-to-menu-from-win").addEventListener("click", (e) =
 })
 
 /////////////////////////// Lose
-function lose(){
+function lose() {
     clearDisplay();
     document.querySelector(".game-over").style.display = "block";
+    document.querySelector(".name-input").style.display = "block";
+    document.querySelector(".name-input").style.background = "#6b1818";
 }
 
 document.querySelector(".restart-lose").addEventListener("click", (e) => {
@@ -480,12 +468,95 @@ document.querySelector(".back-to-menu-from-lose").addEventListener("click", (e) 
     start()
 })
 
-function clearDisplay(){
+function clearDisplay() {
     document.querySelector(".main-menu").style.display = "none";
     document.querySelector(".pause").style.display = "none";
     document.querySelector(".scoreboard-display").style.display = "none";
     document.querySelector(".game-over").style.display = "none";
     document.querySelector(".congratulations").style.display = "none";
+    document.querySelector(".name-input").style.display = "none";
+}
+
+/////////////////////////// get add score
+function postScore() {
+    player = document.querySelector('[name="player-name"]').value
+    if (player == "") {
+        alert("Name can not be empty");
+        return;
+    }
+
+    var score = {
+        player: player,
+        score: GAME_STATE.score,
+        time: GAME_STATE.time,
+    };
+
+
+
+    fetch(API_ADRESS, {
+        method: 'post',
+        body: JSON.stringify(score)
+    }).then(res => console.log(res))
+
+    reset()
+    clearDisplay();
+    document.querySelector(".scoreboard-display").style.display = "block";
+    getScoreBoard(0, player)
+}
+
+function getScoreBoard(skip, player) {
+    fetch(API_ADRESS, {
+        method: 'get'
+    }).then(res => res.json())
+        .then(res => parseScores(res, skip, player));
+}
+
+function parseScores(res, skip, player) {
+    const table = document.querySelector(".scoreboard-table")
+    let tableContent = table.innerHTML
+
+    for (let i = 0; i < res.length; i++) {
+        if (player == res[i].player) {
+            let rank = i + 1
+            console.log(i, res.length)
+            let percentage = (i / res.length) * 100
+
+            let topMessage = document.querySelector(".top-message")
+            topMessage.innerHTML = `Congrats ${player}, you are in the top ${percentage}%, in the ${rank} position`
+            break
+        }
+    }
+
+    for (let i = skip; i < skip + 10; i++) {
+        if (i >= res.length) {
+            break
+        }
+
+        let rank = i + 1
+        let time = formatTime(res[i].time)
+        let percentage = i / 50
+
+        if (player == res[i].player) {
+            let topMessage = document.querySelector(".top-message")
+            topMessage.innerHTML = `Congrats ${player}, you are in the top ${percentage}%, in the ${rank} position`
+        }
+
+        tableContent += '<tr><td>' +
+            rank + '</td><td>' +
+            res[i].player + '</td><td>' +
+            res[i].score + '</td><td>' +
+            time + '</td></tr>'
+    }
+    table.innerHTML = tableContent
+}
+
+function formatTime(seconds) {
+    return [
+        parseInt(seconds / 60),
+        parseInt(seconds % 60)
+    ]
+        .join(":")
+        .replace(/\b(\d)\b/g, "0$1")
 }
 
 start()
